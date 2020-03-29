a539e3eaf59ed21d9f17ec5e2d0432da4c4892c0
Oleg Krymskii <okrymsky@gmail.com>
2020-03-29 11:25:42 +0400

    Add project FileSystem

---
+++ FileSystemProxy.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+
+namespace ItWasMe.FileSystemUtils
+{
+    public class FileSystemProxy : IFileSystemProxy
+    {
+        public Stream Create(string path)
+        {
+            return File.Create(path);
+        }
+
+        public Stream OpenRead(string path)
+        {
+            return File.OpenRead(path);
+        }
+
+        public void Delete(string path)
+        {
+            File.Delete(path);
+        }
+
+        public StreamWriter CreateOrAppendIfExist(string path)
+        {
+            if (File.Exists(path))
+            {
+                return File.AppendText(path);
+            }
+            else
+            {
+                return File.CreateText(path);
+
+            }
+        }
+
+        public void CreateFolderIfNotExist(string folderPath)
+        {
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+        }
+
+        public byte[] ReadAllBytes(string path)
+        {
+            return File.ReadAllBytes(path);
+        }
+    }
+}
---
+++ IFileSystemProxy.cs
@@ -0,0 +1,15 @@
+﻿using System.IO; 
+
+namespace ItWasMe.FileSystemUtils
+{
+    public interface IFileSystemProxy
+    {
+        Stream Create(string path);
+        Stream OpenRead(string path);
+        void Delete(string path);
+        void CreateFolderIfNotExist(string folderPath);
+        byte[] ReadAllBytes(string path);
+        StreamWriter CreateOrAppendIfExist(string path);
+
+    }
+}
